789e5374639f1083bec9d743d99637eaddbe9936
edu3650218 <edu3650218@agderskolen.no>
2026-04-09 11:44:26 +0200

    Fikset Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <nav className="">
-      <h1 className="text-xl font-extrabold tracking-tighter italic">BubbleBubbleGum</h1>
-      <div className="">
-        <Link to="/" className="">Hjem</Link>
-        <Link to="/tjenester" className="">Tjenester</Link>
+    <nav className="bg-linear-to-tl from-pink-300 to-pink-600 text-green-300 flex justify-between p-3">
+      <h1 className="md:text-2xl text-xl font-extrabold tracking-tighter italic">BubbleBubbleGum</h1>
+      <div className="flex gap-4 font-bold md:text-lg mt-0.5">
+        <Link to="/" className="hover:text-green-600 transition duration-100">Hjem</Link>
+        <Link to="/tjenester" className="hover:text-green-600 transition duration-100">Tjenester</Link>
       </div>
     </nav>
   )
 }
+
+
