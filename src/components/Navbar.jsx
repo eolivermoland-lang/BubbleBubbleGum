5749aec7f12743aed04e7f2882486adf921942d0
edu3650218 <edu3650218@agderskolen.no>
2026-04-12 19:21:05 +0200

    fikset navbar på mobil og pc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,17 @@ export default function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="bg-linear-to-tl from-pink-300 to-pink-600 text-green-300 flex justify-between p-4">
+    <nav className="bg-linear-to-tl from-pink-300 to-pink-600 text-green-300 flex justify-between items-center relative p-4">
       <h1 className="md:text-2xl text-xl font-extrabold tracking-tighter italic">BubbleBubbleGum</h1>
-      <div className="gap-4 font-bold md:text-lg mt-0.5 hidden md:flex">
+      <div className="gap-4 font-bold md:text-lg mt-0.5 hidden md:flex ml-auto">
         <Link to="/" onClick={() => setOpen(false)} className="hover:text-green-600 transition duration-100">Hjem</Link>
         <Link to="/tjenester" onClick={() => setOpen(false)} className="hover:text-green-600 transition duration-100">Tjenester</Link>
+        <button className=" hidden text-xl md:flex">🛒</button>
       </div>
 
 
       <div className="flex gap-3">
-        <button className="flex">🛒</button>
+        <button className="md:hidden text-xl flex">🛒</button>
 
         <button 
           className="md:hidden text-xl flex"
@@ -25,11 +26,13 @@ export default function Navbar() {
 
 
       <div
-        className={`md:hidden flex flex-col gap-4 mt-4 transition-all duration-300 ${
-          open ? "block" : "hidden"
+        className={`md:hidden flex flex-col absolute overflow-hidden top-full left-0 w-full transition-all mb-4 bg-linear-to-bl from-pink-300 to-pink-700 duration-300 text-center ${
+          open
+            ? "max-h-40 opacity-100"
+            : "max-h-0 opacity-0"
         }`}
       >
-        <div className="flex gap-2 font-bold text-sm mt-5 flex-col">
+        <div className="flex gap-2 font-bold text-xl mt-4  mb-4 flex-col">
           <Link to="/" className="hover:text-green-600 transition duration-100">Hjem</Link>
           <Link to="/tjenester" className="hover:text-green-600 transition duration-100">Tjenester</Link>
         </div>
