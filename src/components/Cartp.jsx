8645d2c980bf700db6988ae48f11ff4c5823e9ff
edu3650218 <edu3650218@agderskolen.no>
2026-04-22 20:50:51 +0200

    cart design fixed*!

--- src/components/Cartp.jsx
+++ src/components/Cartp.jsx
@@ -5,7 +5,7 @@ export default function Cartp({ cart }) {
   return (
     <section className="">
       <div className="py-10 flex justify-center">
-        <div className="bg-linear-to-tl from-pink-300 to-pink-700 w-[80%] p-6 rounded-xl">
+        <div className="bg-linear-to-tl from-pink-300 to-pink-700 md:w-[60%] w-[80%] p-6 rounded-xl">
 
           <h1 className="font-bold md:text-5xl text-3xl text-green-300 text-center mb-10">🛒 Cart</h1>
 
@@ -14,7 +14,7 @@ export default function Cartp({ cart }) {
           ) : (
             <div className="flex flex-col gap-4">
               {cart.map((item, i) => (
-                <div key={i} className={`p-4 rounded-lg flex justify-between items-center bg-linear-to-tl ${item.color}`}>
+                <div key={i} className={`p-4 rounded-lg flex justify-between items-center md:w-[60%] mx-auto hover:scale-105 transition duration-300 bg-linear-to-tl ${item.color}`}>
                   
                   <div className="text-white">
                     <h2 className="font-bold">{item.name}</h2>
@@ -22,7 +22,7 @@ export default function Cartp({ cart }) {
                     <p className="font-bold">{item.price} kr</p>
                   </div>
 
-                  <img src={item.img} className="w-16 h-16 object-cover rounded" />
+                  <img src={item.img} className="w-16 h-16 object-cover rounded-4xl" />
                 </div>
               ))}
             </div>
@@ -35,7 +35,7 @@ export default function Cartp({ cart }) {
                 Total: {total} kr
               </h2>
 
-              <button className="mt-4 bg-green-500 px-6 py-3 rounded-xl font-bold hover:bg-green-700">
+              <button className="mt-4 bg-black px-6 py-3 rounded-xl md:w-[40%] md:text-xl font-bold hover:bg-gray-800 transition duration-150">
                 Betal nå
               </button>
             </div>
